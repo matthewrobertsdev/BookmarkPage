4710dab6c9b9e78f35b08bb0be36bed1f098289c
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-11-07 16:06:17 -0500

    Updates

---
+++ precache-manifest.cc395cae7e3f5f4c94a88f9dc04be2a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758b41c834f4c4515b0eec16c8f59c28",
+    "url": "/BookmarkPage/index.html"
+  },
+  {
+    "revision": "509092ecaf9ad8de937e",
+    "url": "/BookmarkPage/static/css/main.e6e3da4e.chunk.css"
+  },
+  {
+    "revision": "c79af83a77614d7a9413",
+    "url": "/BookmarkPage/static/js/2.d046d9cd.chunk.js"
+  },
+  {
+    "revision": "509092ecaf9ad8de937e",
+    "url": "/BookmarkPage/static/js/main.a7e68bdc.chunk.js"
+  },
+  {
+    "revision": "9fb6f9a7a0ff120df928",
+    "url": "/BookmarkPage/static/js/runtime-main.21d1cb94.js"
+  }
+]);
